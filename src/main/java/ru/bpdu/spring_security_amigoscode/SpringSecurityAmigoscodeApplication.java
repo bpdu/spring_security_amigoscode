f942db0445c7e58670f372a7afa37c122f7b5a50
Alexey Rumyantsev <me@bpdu.ru>
2022-07-13 19:07:12 +0300

    Initial commit

---
+++ src/main/java/ru/bpdu/spring_security_amigoscode/SpringSecurityAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package ru.bpdu.spring_security_amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAmigoscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bpdu/spring_security_amigoscode/SpringSecurityAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bpdu.spring_security_amigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
